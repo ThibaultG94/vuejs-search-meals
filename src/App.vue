2917054a0506658c886ba6118576388d0b54332e
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-03-09 12:10:24 +0100

    router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-	<h1 class="bg-blue-500">App.vue</h1>
+	<router-view />
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
+import router from './router';
+import './assets/main.css';
 import App from './App.vue';
 
-import './assets/main.css';
-
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
